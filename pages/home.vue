34e926129985f750dfd625e7ac167cbbb0ae2451
WangXiaoXi <995107408@qq.com>
2018-08-09 22:37:53 +0800

    加下 eslint 测试

---
+++ pages/home.vue
@@ -0,0 +1,35 @@
+{{{{raw}}}}
+<template>
+  <div class="home">
+    <span style="display:block;margin-top:20px">测试专用</span>
+  </div>
+</template>
+{{{{raw}}}}
+<script type="text/ecmascript-6">
+  export default {
+    created () {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    mounted () {
+      console.log(12)
+    },
+    components: {
+
+    },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
